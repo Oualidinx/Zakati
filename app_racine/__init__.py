4def91f429181a372a2fd73041c9904c078c5e79
OUAREM Oualid <oua1992@gmail.com>
2020-08-18 22:53:00 +0100

    help2

--- app_racine/__init__.py
+++ app_racine/__init__.py
@@ -9,16 +9,16 @@ from flask_bcrypt import Bcrypt
 app = Flask(__name__)
 db_parameters={
     'driver' : 'mysql',
-    'user' : 'oualid',
-    'password': '',
-    'host':'localhost',
+    'user' : 'ynohxpjmaejjnz',
+    'password': '8b77a9c380c0c34308924eb8aa07a480d6fef9be579314ea5900b8ee95c4ef9a',
+    'host':'ec2-54-91-178-234.compute-1.amazonaws.com',
     'db_name':'database',
     'charset' :'utf8'
 }
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "{driver}://{user}:{password}@{host}/{db_name}?charset={charset}".format(**db_parameters)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://ynohxpjmaejjnz:8b77a9c380c0c34308924eb8aa07a480d6fef9be579314ea5900b8ee95c4ef9a@ec2-54-91-178-234.compute-1.amazonaws.com:5432/d2h6pid7rn64nd" #"{driver}://{user}:{password}@{host}/{db_name}?charset={charset}".format(**db_parameters)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
