7bb1bc90c0d7e8e10ee2e7a0510eb7c8be18b809
OUAREM Oualid <oua1992@gmail.com>
2020-08-26 17:17:30 +0100

    db config for heroku

--- app_racine/__init__.py
+++ app_racine/__init__.py
@@ -8,16 +8,16 @@ from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 db_parameters={
-    'driver' : 'mysql',
-    'user' : 'oualid',
-    'password': '',
-    'host':'localhost',
-    'db_name':'database',
-    'charset' :'utf8'
+    'driver' : 'postgres',
+    'port': 5432,
+    'user' : 'ynohxpjmaejjnz',
+    'password': '8b77a9c380c0c34308924eb8aa07a480d6fef9be579314ea5900b8ee95c4ef9a',
+    'host':'ec2-54-91-178-234.compute-1.amazonaws.com',
+    'db_name':'d2h6pid7rn64nd'
 }
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 #app.config['SQLALCHEMY_DATABASE_URI'] = "postgres://ynohxpjmaejjnz:8b77a9c380c0c34308924eb8aa07a480d6fef9be579314ea5900b8ee95c4ef9a@ec2-54-91-178-234.compute-1.amazonaws.com:5432/d2h6pid7rn64nd" 
-app.config['SQLALCHEMY_DATABASE_URI'] = "{driver}://{user}:{password}@{host}/{db_name}?charset={charset}".format(**db_parameters)
+app.config['SQLALCHEMY_DATABASE_URI'] = "{driver}://{user}:{password}@{host}:{port}/{db_name}".format(**db_parameters)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 db = SQLAlchemy(app)
