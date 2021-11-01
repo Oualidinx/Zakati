11d9566715ff0b08f07320d5ef84937b90a4f73b
OUAREM Oualid <oua1992@gmail.com>
2021-11-01 17:16:32 +0100

    hello

--- run.py
+++ run.py
@@ -3,8 +3,8 @@ import os
 from app_racine import app, create_app
 
 from dotenv import load_dotenv
-#load_dotenv('.flaskenv')
-app = create_app('production')
+load_dotenv('.flaskenv')
+app = create_app(os.environ.get('FLASK_ENV'))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")  # host="0.0.0.0", port="5050"
