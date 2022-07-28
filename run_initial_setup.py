54cecbe304a84d137527e036c73386a85236f96a
Oualidinx <oua1992@gmail.com>
2022-07-28 18:03:32 +0100

    install critere table in initial setup

--- run_initial_setup.py
+++ run_initial_setup.py
@@ -4,12 +4,12 @@ import secrets
 from werkzeug.security import generate_password_hash
 
 from app_racine import database
-from app_racine.master.models import Wilaya
+from app_racine.master.models import Wilaya, Critere
 from app_racine.users.models import User
 
 import os
 # ajouter les wilayas
-f = open("wilaya.json")
+f = open("uploads/wilaya.json")
 print("Adding all wilayas...", end="")
 twilaya = json.load(f)
 wilayas = [Wilaya(name=instance['name'], zip_code=instance['zip_code']) for instance in twilaya]
@@ -18,6 +18,13 @@ for w in wilayas:
     database.session.commit()
 print("done")
 
+print("adding criteria")
+f = open("uploads/critere.json")
+tcriteria = json.load(f)
+tcriteria = [
+    dict(label = v[1], category = v[2], weight=v[3]) for v in tcriteria['values']
+]
+criteria = [Critere(label = instance['label'], category = instance['category'], weight = instance['weight']) for instance in tcriteria]
 # adding the admin user
 print("adding the admin...", end="")
 
